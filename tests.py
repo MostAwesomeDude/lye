20b0ffc6a4f77ff196356ab20f2675c9a98a9c34
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-02-05 20:11:46 -0800

    Add tests for Note.

--- tests.py
+++ tests.py
@@ -2,10 +2,22 @@ import unittest
 
 import lye
 
-class Test(unittest.TestCase):
+class TestNote(unittest.TestCase):
 
     def test_note(self):
-        note = lye.LyGrammar("c1").apply("note")
+        note, error = lye.LyGrammar("c").apply("note")
+        self.assertEqual(note.pitch, 48)
+        self.assertEqual(note.duration, 120)
+
+    def test_whole_note(self):
+        note, error = lye.LyGrammar("c1").apply("note")
+        self.assertEqual(note.duration, 480)
+
+    def test_es(self):
+        note, error = lye.LyGrammar("es").apply("note")
+        self.assertEqual(note.pitch, 51)
+
+class Test(unittest.TestCase):
 
     def test_measure(self):
         measure = lye.LyGrammar("c e g c' |").apply("measure")
