f7fb075fd55ca66e3a486c5beddb68900cdbdf47
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-03-11 01:25:25 -0800

    song: Instruments.

--- lye/song.py
+++ lye/song.py
@@ -1,5 +1,6 @@
 from StringIO import StringIO
 
+from lye.instruments import numbered_instruments
 from lye.MidiFile import MIDIFile
 
 class Song(object):
@@ -9,8 +10,20 @@ class Song(object):
 
     ticks_per_beat = 480
 
-    def __init__(self, *melodies):
-        self.melodies = melodies
+    def __init__(self):
+        self.melodies = {}
+
+    def add_melody(self, melody, channel, instrument):
+        """
+        Add a melody to this song.
+
+        It is up to the caller to avoid clobbering channels; this method will
+        gladly replace a melody with another melody given the chance.
+
+        The instrument may be None if the channel is 10 (drums).
+        """
+
+        self.melodies[channel] = melody, instrument
 
     def to_midi(self):
         f = MIDIFile(len(self.melodies), ticksPerBeat=self.ticks_per_beat)
@@ -21,7 +34,11 @@ class Song(object):
         f.addTrackName(track, time, "Lye")
         f.addTempo(track, time, 84 / 4)
 
-        for channel, melody in enumerate(self.melodies):
+        for channel in self.melodies:
+            melody, instrument = self.melodies[channel]
+            if instrument in numbered_instruments:
+                f.addProgramChange(track, channel, time,
+                    numbered_instruments[instrument])
             melody.to_midi(f, channel)
 
         sio = StringIO()
