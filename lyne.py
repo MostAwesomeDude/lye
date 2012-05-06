366ca8d63b120d7486be1a7a7cd1ee0c5d2f4606
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-05-06 12:04:49 -0700

    Modify lyne.py to handle other libraries, and start oleo.

--- lyne.py
+++ lyne.py
@@ -3,10 +3,10 @@ import sys
 from lye.timelyne import Timelyne
 from lye.library import Library
 
-library = Library("groove")
+library = Library(sys.argv[1])
 
-with open(sys.argv[1], "rb") as f:
+with open(sys.argv[2], "rb") as f:
     lyne = Timelyne.from_lines(library, f)
     data = lyne.to_midi()
-    with open(sys.argv[2], "wb") as out:
+    with open(sys.argv[3], "wb") as out:
         out.write(data)
