a1dd60f3aaa81742fa57d6d831069daa7ea1d7c3
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-05-14 07:37:32 -0700

    lyne.py: Add shebang, usage.

--- lyne.py
+++ lyne.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python
+
 import sys
 
 from lye.library import Library
 from lye.lyne import Timelyne
+
+if len(sys.argv) < 4:
+    print "Usage:", sys.argv[0], "<library> <lyne> <midi>"
+    sys.exit(1)
 
 library = Library(sys.argv[1])
 
