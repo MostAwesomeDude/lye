2d18dd6d7d8c71b79fcf9eb06aed36ba2089e27a
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-06-23 15:39:03 -0700

    Visitor: Add a peephole applier.

--- Text/Lye/Visitor.hs
+++ Text/Lye/Visitor.hs
@@ -22,3 +22,12 @@ longestChord = let
     f (Chord xs) is = maximum $ length xs:is
     f x is = maximum $ 0:is
     in para f
+
+applyPeephole :: ([Expression] -> [Expression]) -> Expression -> Expression
+applyPeephole f = let
+    g (Chord es) = Chord $ f es
+    g (Music es) = Music $ f es
+    g (Voice es) = Voice $ f es
+    g (Voices es) = Voices $ f es
+    g e = e
+    in transform g
