ea58300f8cbb8153bb8b80fd1fad2251dcc52011
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-11-03 14:17:41 -0700

    visitors/pegs: Switch to Parsley here too.

--- lye/visitors/pegs.py
+++ lye/visitors/pegs.py
@@ -1,23 +1,29 @@
-from pymeta.grammar import OMeta
+from parsley import makeGrammar
 
 from lye.ast import CLOSE_SLUR, OPEN_SLUR, Slur
 from lye.visitors.visitor import hasfield, Visitor
 
+slur_globals = {
+    "OPEN_SLUR": OPEN_SLUR,
+    "CLOSE_SLUR": CLOSE_SLUR,
+    "Slur": Slur,
+}
+
 slur_maker = """
-open  ::= <exactly OPEN_SLUR>
-close ::= <exactly CLOSE_SLUR>
-not_close ::= <anything>:x ?( x is not CLOSE_SLUR ) => x
-slur  ::= <anything>:x <open> <not_close>+:xs <close> => Slur([x] + xs)
-slurify ::= (<slur> | <anything>)*
+open  = exactly(OPEN_SLUR)
+close = exactly(CLOSE_SLUR)
+not_close = anything:x ?(x is not CLOSE_SLUR) -> x
+slur  = anything:x open not_close+:xs close -> Slur([x] + xs)
+slurify = (slur | anything)*
 """
 
+_slurry = makeGrammar(slur_maker, slur_globals)
+
 class SlurMaker(Visitor):
-    class Inner(OMeta.makeGrammar(slur_maker, globals())):
-        pass
 
     def visit_generic(self, node):
         if hasfield(node, "exprs"):
-            exprs, error = self.Inner(node.exprs).apply("slurify")
+            exprs = _slurry(node.exprs).slurify()
             node = node._replace(exprs=exprs)
 
         return node, True
