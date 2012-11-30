eb15a9e65636334ca4572bdab6aab13c8ff74de4
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-11-29 22:10:14 -0800

    Update lye2ast.

--- lye2ast.py
+++ lye2ast.py
@@ -2,27 +2,32 @@
 
 import sys
 
+from lye.combyne import Bynder
 from lye.grammar import LyeGrammar
 from lye.pretty import pretty
-from lye.visitors import express_ast, simplify_ast
+from lye.visitors import simplify_ast
 
 if len(sys.argv) < 2:
     print "Usage:", sys.argv[0], "<snippet> [<instrument>]"
     sys.exit(0)
+
+instrument = None
+
+if len(sys.argv) >= 3:
+    instrument = " ".join(sys.argv[2:])
 
 with open(sys.argv[1], "rb") as f:
     s = f.read()
 
 g = LyeGrammar(s)
 ast = g.lye()
-ast = simplify_ast(ast)
+bound = Bynder(simplify_ast(ast), instrument)
 
 print "AST:"
-print pretty(ast)
+print pretty(bound)
 
-if len(sys.argv) >= 3:
-    instrument = " ".join(sys.argv[2:])
-    ast = express_ast(ast, instrument)
+print "AST (%s):" % instrument
+print pretty(bound.specialized())
 
-    print "AST (%s):" % instrument
-    print pretty(ast)
+print "Scheduled:"
+print pretty(bound.schedule())
