a91ae240b8ecc36391490c40ae21f779a6f31165
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-12-05 20:29:09 -0800

    facts: Unbreak in the obvious way.
    Fuck datalog.

--- lye/facts.py
+++ lye/facts.py
@@ -1,23 +1,4 @@
 #import re
 
-def can_legato(x): return False
-
-'''
-from pyDatalog.pyDatalog import Datalog_engine
-
-pl = Datalog_engine()
-
-pl.load("""
-+ legato(acoustic_guitar__nylon_)
-+ legato(acoustic_guitar__steel_)
-+ legato(electric_guitar__jazz_)
-+ legato(electric_guitar__clean_)
-+ legato(electric_guitar__muted_)
-+ legato(overdriven_guitar)
-+ legato(distorted_guitar)
-""")
-
-def can_legato(instrument):
-    i = re.sub("[() ]", "_", instrument)
-    return bool(pl.ask("legato(%s)" % i))
-'''
+def can_legato(x):
+    return "guitar" in x
