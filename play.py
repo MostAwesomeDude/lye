2c492832342e70e5d6dd75ed3b0da691e1262f1d
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-12-06 01:06:07 -0800

    play: Export most of the setup for playing.

--- play.py
+++ play.py
@@ -2,37 +2,28 @@
 
 import sys
 
-from lye.combyne import Bynder, Combyned, Drumlyne
-from lye.grammar import LyeGrammar
-from lye.tiedye import play_a_bynder
-from lye.utilities import find_instrument
-from lye.visitors import simplify_ast
+from twisted.internet import reactor
+from twisted.python.filepath import FilePath
+from twisted.python.reflect import namedAny
 
-if len(sys.argv) < 3:
-    print "Usage:", sys.argv[0], "(<instrument> <snippet>)+"
+from lye.lybrary import Lybrary
+from lye.tiedye import play_a_bynder
+
+if len(sys.argv) < 2:
+    print "Usage:", sys.argv[0], "<song>"
     sys.exit(0)
 
-pairs = zip(sys.argv[1::2], sys.argv[2::2])
+print "Loading..."
 
-bound = Combyned()
-for instrument, path in pairs:
-    instrument = find_instrument(instrument)
-    with open(path, "rb") as f:
-        s = f.read()
-        g = LyeGrammar(s)
-        ast = g.lye()
-        if instrument == "drums":
-            bound |= Drumlyne(simplify_ast(ast))
-        else:
-            bound |= Bynder(simplify_ast(ast), instrument)
+song = namedAny(sys.argv[1])
+
+bound = song(Lybrary(FilePath("/home/simpson/lybrary")))
 
 print "Playing..."
 
-bound.tempo = 120
 bound.ticks_per_beat = 120
 
 ml = play_a_bynder(bound, "gugs.sf2")
 
-from twisted.internet import reactor
 reactor.callWhenRunning(ml.start, reactor)
 reactor.run()
